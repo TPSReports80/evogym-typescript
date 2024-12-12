6efc74c181f19f38a2dbf502d913e893a93f2773
TPSReports80 <mikejoo80@gmail.com>
2024-12-11 22:49:35 -0800

    added pages

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -1,13 +1,20 @@
+import { SelectedPage } from "@/share/types";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
   page: string;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
 };
 
-function Link({ page }: Props) {
-  const lowerCasePage = page.toLowerCase().replace(" ", "");
+function Link({ page, selectedPage, setSelectedPage }: Props) {
+  const lowerCasePage = page.toLowerCase().replace(" ", "") as SelectedPage;
   return (
-    <AnchorLink href={`#${lowerCasePage}`} onClick={} className="">
+    <AnchorLink
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+      className={`transition duration-500 hover:text-primary-300 ${selectedPage === lowerCasePage ? "text-primary-500" : ""}`}
+    >
       {page}
     </AnchorLink>
   );
